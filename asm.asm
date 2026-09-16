a5446a55a192e102a273396927e6ad7cab65467b
ElijahM <161932463+Scooterking38@users.noreply.github.com>
2026-09-16 12:11:42 +0100

    Update asm.asm

--- asm.asm
+++ asm.asm
@@ -9,18 +9,9 @@ check_age:
     cmp rcx, 18
     jg .accepted
 
-    lea rax, [rel rejected]
+    mov rax 0
     ret
 
 .accepted:
-    lea rax, [rel accepted]
+    mov rax, 1
     ret
-
-
-section .rdata
-
-accepted:
-    db "Allowed to drink! yay!", 0
-
-rejected:
-    db "Not allowed to drink! arrest him!", 0
